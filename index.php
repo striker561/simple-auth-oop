572555ac2a5caf03a48bb9fce8da98f5abbaab54
TemmyJewel <temmyjewel22@gmail.com>
2026-02-18 15:19:59 +0100

    index changes

--- index.php
+++ index.php
@@ -1,14 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
-</head>
-<body>
-    <ul>
-        <li><a href="views/login.php">Login</a></li>
-        <li><a href="views/register.php">Register</a></li>
-    </ul>
-</body>
-</html>
+
+
+<?php
+header("Location: views/login.php");
+
+?>
